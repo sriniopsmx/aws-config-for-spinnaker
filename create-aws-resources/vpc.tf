602c56b5e070895344e9e62baf56ca69d9d14a74
keerthibingi <keerthi.bingi@opsmx.io>
2024-08-14 00:02:41 -0700

    Added subnet for VPC

--- create-aws-resources/vpc.tf
+++ create-aws-resources/vpc.tf
@@ -1,12 +1,7 @@
 
-# Configure AWS Provider with your credentials
-#provider "aws" {
-#  region = "us-east-1" # Replace with your desired region
-#}
-
 # Create a VPC
 resource "aws_vpc" "main" {
-  cidr_block = "172.31.32.0/20" # "10.0.0.0/16" # Replace with your desired CIDR block
+  cidr_block = "172.31.32.0/20" # Replace with your desired CIDR block
 
   # Set the immutable metadata tag for "purpose"
   tags = {
@@ -14,7 +9,22 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Output the VPC ID
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.31.32.0/24" # Replace with your desired CIDR block
+
+  # Set the immutable metadata tag for "purpose"
+  tags = {
+    immutable_metadata="{\"purpose\":\"spin-subnet\"}"
+  }
+}
+
+
+# Output the VPC and Subnet IDs
 output "vpc_id" {
   value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.main.id
 }
